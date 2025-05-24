f07a17a2743804152c7219433bdee936d81d6674
deneb <a54556306@gmail.com>
2025-05-24 19:48:22 +0900

    model_books and accounts, views.py_books

--- back/accounts/models.py
+++ back/accounts/models.py
@@ -1,7 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from books.models import Category, Book
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    # many to many field
+    followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
+
+    # foreign key
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+    # table
+    nickname = models.CharField(max_length=20)
+    user_id = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, null=True, blank=True)
+    gender = models.CharField(max_length=20) # 선택할 수 있는 field로?
+    age = models.IntegerField()
+    weekly_age_reading_time = models.IntegerField()
+    annual_reading_amount = models.IntegerField()
+    profile_img = models.CharField(max_length=200)
+
+
+
--- back/accounts/views.py
+++ back/accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
 # Create your views here.
+
+# serialize 전 코드
+# @login_required
+# def follow(request, user_pk):
+#     User = get_user_model()
+#     person = User.objects.get(pk=user_pk)
+#     if person != request.user:
+#         if request.user in person.followers.all():
+#             person.followers.remove(request.user)
+#         else:
+#             person.followers.add(request.user)
+#     return redirect('accounts:profile', person.username)
+
+
+
+
